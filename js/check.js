3067b7c2bfc0ead768504d3b2c7def6ad634e39d
Macko Martin <macko.mar@gmail.com>
2013-11-19 13:06:57 +0100

    Cteni s porozumenim Aj 1-2, js checking, bootstrap styling

---
+++ js/check.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+	//countdown setting
+	$('#countdown').countdown({
+		until: "+45m",
+		onExpiry: function () {
+			$(":submit").prop('disabled', 'true');
+			check_answers();
+		}});
+	//submit event
+	$(":submit").on('click', function (e) {
+		e.preventDefault();
+		$('li').removeClass('alert-danger alert-success');
+		check_answers();
+	});
+
+	//checking function
+	function check_answers() {
+		$(":checked").each(function () {
+			_this = $(this);
+			console.log(_this.attr('correct'));
+			if(_this.attr('correct')!= undefined) {
+				_this.closest('li').addClass('alert-success');
+			} else {
+				_this.closest('li').addClass('alert-danger');
+			}
+		})
+		$("html, body").animate({ scrollTop: 0 }, "slow");
+	}
+});
