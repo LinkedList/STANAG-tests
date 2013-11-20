63b8b75d4218427c6febfbd7dc3006bcb0613858
Macko Martin <macko.mar@gmail.com>
2013-11-20 12:48:14 +0100

    nj cteni s porozumenim 1-2, right answer higlight

--- js/check.js
+++ js/check.js
@@ -16,13 +16,21 @@ $(document).ready(function () {
 
 	//checking function
 	function check_answers() {
-		$("#Quiz input[type='radio']:checked").each(function () {
+		$("#Quiz input[type='radio']").each(function () {
 			var _this = $(this);
 			var attr = _this.attr('correct');
+
+			if(_this.is(":checked")) {
+				if(typeof attr !== 'undefined' && attr !== false) {
+					_this.closest('li').addClass('alert-success');
+				} else {
+					_this.closest('li').addClass('alert-danger');
+				}	
+			}
+
+			//show right answer
 			if(typeof attr !== 'undefined' && attr !== false) {
-				_this.closest('li').addClass('alert-success');
-			} else {
-				_this.closest('li').addClass('alert-danger');
+				_this.closest('li').append(' <span class="glyphicon glyphicon-ok"></span>')	
 			}
 		})
 		$("html, body").animate({ scrollTop: 0 }, "slow");
